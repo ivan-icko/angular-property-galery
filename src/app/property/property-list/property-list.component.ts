9223716c796dd1763ad8461cfd3a0d18737ada5f
ivan-icko <ivanstepanovic6@gmail.com>
2022-07-18 21:15:10 +0200

    Kreiranje dugmica na slikama

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> = [
+    {
+    "Id":1,
+    "Name":"Ivanova Kuca",
+    "Type":"House",
+    "Price":12000
+  },{
+    "Id":2,
+    "Name":"Mikijeva kuca",
+    "Type":"House",
+    "Price":12000
+  },{
+    "Id":3,
+    "Name":"Perina kuca",
+    "Type":"House",
+    "Price":12000
+  },{
+    "Id":4,
+    "Name":"Birla House",
+    "Type":"House",
+    "Price":12000
+  },{
+    "Id":5,
+    "Name":"Birla House",
+    "Type":"House",
+    "Price":12000
+  }
+]
   constructor() { }
 
   ngOnInit(): void {
